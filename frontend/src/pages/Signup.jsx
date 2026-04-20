324bfcb5c06d7fa1f59f08b4ac7b733b30ac5f4d
Mamta Saini <mamtasaini@Mamtas-MacBook-Air.local>
2026-04-20 18:32:26 +0530

    fix SPA routing

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -74,6 +74,7 @@ const Signup = () => {
         </div>
 
         <button
+        type="button"
           onClick={handleSignup}
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg font-semibold transition"
         >
